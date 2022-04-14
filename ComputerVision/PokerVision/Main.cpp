89202b6b7acc7037ffeb92348255343b5a2b2918
Antoine Pavy <antoine.pavy92@gmail.com>
2022-04-14 16:17:23 +0200

    Added json parser

--- ComputerVision/PokerVision/Main.cpp
+++ ComputerVision/PokerVision/Main.cpp
@@ -11,6 +11,10 @@
 #include "Card.h"
 #include "PokerVision.h"
 
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+
 int main(int argc, char** argv)
 {
 	std::string windowName = "card";
