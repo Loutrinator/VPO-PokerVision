234afc784bb18832e722312c1b1fd6db56c2e472
Aedrinios <clementclerc2@gmail.com>
2022-04-21 19:19:20 +0200

    added: iterate on all files in resources & configuration + check card position

--- Benchmark/Benchmark.cpp
+++ Benchmark/Benchmark.cpp
@@ -1,36 +1,113 @@
 // Benchmark.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <fstream>
 #include <iostream>
 #include <filesystem>
-#include <fstream>
 #include <Windows.h>
 #include <sstream>
 #include <string>
 #include <codecvt>
 #include <locale>
+#include <cmath>
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
 
+float threshold = 400;
+
+bool comparePoints(json point1, json point2) {
+	std::vector<float> card1 = { point1[0]["x"], point1[0]["y"], point1[1]["x"],point1[1]["y"],
+								point1[2]["x"], point1[2]["y"], point1[3]["x"], point1[3]["y"]};
+	std::vector<float> card2 = { point2[0]["x"], point2[0]["y"], point2[1]["x"],point2[1]["y"],
+								point2[2]["x"], point2[2]["y"], point2[3]["x"], point2[3]["y"] };
+
+	std::sort(card1.begin(), card1.end());
+	std::sort(card2.begin(), card2.end());
+	for (int i = 0; i < card1.size(); i++) {
+		if (std::abs(card1[i] - card2[i]) > 400) {
+			return false;
+		}
+	}
+	return true;
+}
+
 int main()
 {
-	std::string config;
-	std::string imagePath;
-	std::cout << "Choose config 1,2 or 3" << std::endl;
-	std::cin >> config;
-	std::cout << "Choose image to load" << std::endl;
-	std::cin >> imagePath;
-	
-	std::ifstream user_file( "resources/" + imagePath + ".json", std::ifstream::binary);
-	if (user_file.fail() ){
-		std::cout << "User File does not exist :(";
-		return 0;
+	std::string path = "resources/";
+	std::string filename;
+	for (const auto& entry : std::filesystem::directory_iterator(path)) {
+		if (entry.path().extension().u8string() == ".json") {
+			filename = entry.path().stem().u8string();
+		}
+		else {
+			continue;
+		}
+		for (int i = 1; i < 4; i++) {
+			std::string outputName = "measures_config" + std::to_string(i) + "_" + filename;
+			std::ifstream config_file("../output/" + outputName + ".json", std::ifstream::binary);
+
+			std::ifstream user_file("resources/" + filename + ".json", std::ifstream::binary);
+			if (user_file.fail()) {
+				std::cout << "User File does not exist :(";
+				return 0;
+			}
+
+			json userJson = json::parse(user_file);
+			json resultJson = json::parse(config_file);
+
+			bool foundCard = false;
+			int goodCardCount = 0;
+			int badCardCount = 0;
+			for (int x = 0; x < resultJson["cards"].size(); x++) {
+				for (int y = 0; y < userJson["cards"].size(); y++) {
+					if (resultJson["cards"][x]["rank"] == userJson["cards"][y]["rank"]
+						&& resultJson["cards"][x]["suit"] == userJson["cards"][y]["suit"]
+						/* && userJson["cards"][i]["group"] == resultJson["cards"][j]["group"]*/
+						&& comparePoints(resultJson["cards"][x]["points"], userJson["cards"][y]["points"])) {
+						goodCardCount++;
+						foundCard = true;
+						break;
+					}
+				}
+				if (!foundCard)
+					badCardCount++;
+				foundCard = false;
+			}
+
+			json benchmark;
+			benchmark["Goodmatch"] = goodCardCount;
+			benchmark["totalcards"] = userJson["cards"].size();
+
+			benchmark["Badmatch"] = badCardCount;
+			benchmark["percentage"] = (float)goodCardCount / (float)userJson["cards"].size();
+			benchmark["executiontime"] = resultJson["totalTimespan"];
+			benchmark["cardsearchtime"] = resultJson["findCardsTimespan"];
+			benchmark["grouptime"] = resultJson["groupingTimespan"];
+			benchmark["overlaptime"] = resultJson["removeOverlapTimespan"];
+
+			std::string filename("../output/" + outputName + "_benchmarked.json");
+			std::ofstream outfile;
+
+			outfile.open(filename, std::ofstream::out | std::ofstream::trunc);
+
+			if (!outfile.is_open()) {
+				std::cerr << "failed to open " << filename << '\n';
+			}
+			else {
+				outfile.write(benchmark.dump(4).c_str(), benchmark.dump(4).size());
+				std::cerr << "Done Writing!" << std::endl;
+			}
+		}
+		
 	}
+
+
 	
-	std::wstring cmdArgslistSetChannel = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(config);
-	cmdArgslistSetChannel += L" " + std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(imagePath);
 
 	//Crash with opencv in PokerVision.exe
+
+	//std::wstring cmdArgslistSetChannel = std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(config);
+	//cmdArgslistSetChannel += L" " + std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(imagePath);
 
 	/*HANDLE hProcess;
 	HANDLE hThread;
@@ -60,57 +137,9 @@ int main()
 		CloseHandle(pi.hProcess);
 	}
 	catch (std::exception e){}*/
-
-	std::string outputName = "measures_config" +config + "_" + imagePath;
-	std::ifstream config_file("../output/" + outputName + ".json", std::ifstream::binary);
-	
-	json userJson = json::parse(user_file);
-	json resultJson = json::parse(config_file);
-
-	bool foundCard = false;
-	int goodCardCount = 0;
-	int badCardCount = 0;
-	for (int i = 0; i < resultJson["cards"].size(); i++) {
-		for (int j = 0; j < userJson["cards"].size(); j++) {
-			if (resultJson["cards"][i]["rank"] == userJson["cards"][j]["rank"]
-				&& resultJson["cards"][i]["suit"] == userJson["cards"][j]["suit"]
-				/* && userJson["cards"][i]["group"] == resultJson["cards"][j]["group"]*/) {
-				//json userPoints = userJson["cards"][i]["points"];
-				//json resultPoints = resultJson["cards"][j]["points"];
-				goodCardCount++;
-				foundCard = true;
-				break;
-			}
-		}
-		if(!foundCard)
-			badCardCount++;
-		foundCard = false;
-	}
-
-	json benchmark;
-	benchmark["Goodmatch"] = goodCardCount;
-	benchmark["totalcards"] = userJson["cards"].size();
-
-	benchmark["Badmatch"] = badCardCount;
-	benchmark["percentage"] = (float)goodCardCount / (float)userJson["cards"].size();
-	benchmark["executiontime"] = resultJson["totalTimespan"];
-	benchmark["cardsearchtime"] = resultJson["findCardsTimespan"];
-	benchmark["grouptime"] = resultJson["groupingTimespan"];
-	benchmark["overlaptime"] = resultJson["removeOverlapTimespan"];
-
-	std::string filename("../output/" + outputName+"_benchmarked.json");
-	std::ofstream outfile;
-
-	outfile.open(filename, std::ofstream::out | std::ofstream::trunc);
-
-	if (!outfile.is_open()) {
-		std::cerr << "failed to open " << filename << '\n';
-	}
-	else {
-		outfile.write(benchmark.dump(4).c_str(), benchmark.dump(4).size());
-		std::cerr << "Done Writing!" << std::endl;
-	}
 }
+
+
 
 
 
